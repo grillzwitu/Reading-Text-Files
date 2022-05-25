9b9c659c3e6f3b0d33fbe3ae9920a43c893100c1
grillzwitu <grillzwitu@gmail.com>
2022-05-25 15:07:55 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Reading-Text-Files
+
+This repository contains a python script that opens and reads the contents of a text file, counts the number of words in the text file and stores words and their count in a dictionary.
---
+++ main.py
@@ -0,0 +1,37 @@
+# Read text from a file, and count the occurence of words in that text
+# Example:
+# count_words("The cake is done. It is a big cake!") 
+# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
+
+def read_file_content(filename):
+    # [assignment] Add your code here
+
+    file = open(filename)
+    file_content = file.read()
+
+    return file_content
+
+
+def count_words():
+    text = read_file_content("./story.txt")
+    # [assignment] Add your code here
+
+    word_dict = {}
+
+    split_text = text.replace(',','').replace('?', '').replace('.', '').split()
+
+    for word in split_text:
+
+        if word in word_dict:
+            count = word_dict[word]
+        else:
+            count = 0
+
+        word_dict[word] = count + 1
+
+    print(word_dict)
+
+    return word_dict
+
+
+count_words()
